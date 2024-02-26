dfe7afee6382b69f746be3d41eb2b73718f92262
Parag Chauhan <iamparag121298@gmail.com>
2024-02-26 22:37:01 +0530

    changes

--- src/component/GameBoard.jsx
+++ src/component/GameBoard.jsx
@@ -1,22 +1,41 @@
-const initialGameBoard=[
-    [null,null,null]
-    [null,null,null]
-    [null,null,null]
-];
-export default function GameBoard(){
-    return(
-        <ol id="game-board">
-            {initialGameBoard.map((row,rowIndex)=>(
-                <li key={rowIndex}>
-                    <ol>
-                        {row.map((col,colIndex)=>(
-                        <li key={colIndex}>
-                            <button>X</button>
-                        </li>
-                        ))}
-                    </ol>
-                </li>
-                ))}
-        </ol>
-    )
+import { useState } from "react";
+
+
+export default function GameBoard({ onSelectSquare, board }) {
+ 
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     console.log(updatedBoard)
+  //     return updatedBoard;
+
+  //   });
+
+  //   onSelectSquare();
+  // }
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react"
-export default function Player({name, symbol}){
+export default function Player({name, symbol, isActive, onChangeName}){
     const [input, setInput]=useState(false)
     const [playerName, setPlayerName]=useState(name)
 
    function handleClick(){
         setInput((edit)=> !edit)
+
+        if(input){
+            onChangeName(symbol, playerName)
+        }
     }
 
    function handleChange(event){
     setPlayerName(event.target.value)
 }
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {!(input) && <span className="player-name">{playerName}</span>}
                 {(input) && <input value={playerName} onChange={handleChange}/>}
